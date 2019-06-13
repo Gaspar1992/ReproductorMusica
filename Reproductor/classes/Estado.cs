6633f0ac6ad40a59c679fd2182bfd1f87efd46f2
Gaspar Rodriguez Valero <Gaspar1992@users.noreply.github.com>
2019-06-13 20:29:21 +0200

    Comprobado la compilación de la instalación, añadido estado recovery y logs

---
+++ Reproductor/classes/Estado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reproductor.classes
+{
+    class Estado
+    {
+        public string NombreLista { get; set; }
+        public string NombreCancion { get; set; }
+        public double PuntoCancion { get; set; }
+    }
+}
